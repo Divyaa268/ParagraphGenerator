ecb4b2c0ae001015b24f811b7792a79a51b69c4c
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-05-15 17:17:04 +0530

    Added JavaScript Code.
    Added JS functions to generate words and paragraphs.

---
+++ index.js
@@ -0,0 +1,47 @@
+const input = document.getElementById("numOfWords");
+const container = document.querySelector(".container");
+
+
+const generateWords = (n) => {
+
+    let text = "";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for(let i =0 ;i <n ; i++)
+    {
+        const random = (Math.random() * letters.length-1).toFixed(0);
+        text += letters[random];
+    }
+    return text;
+
+}
+
+// console.log(generateWords(5));
+
+
+
+let numOfWords;
+const generatePara = () =>
+{
+    // console.log(Number(input.value));
+   numOfWords =  Number(input.value);
+
+   const para = document.createElement("p");
+
+   let data = "";
+
+   for(let i=0;i<numOfWords;i++)
+   {
+    const randomNumber = (Math.random() *15).toFixed(0);
+
+    data += generateWords(randomNumber);
+    data += " ";
+   }
+
+   para.innerText = data;
+
+   para.setAttribute("class", "paras");
+
+   container.append(para)
+
+}
